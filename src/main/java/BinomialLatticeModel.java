3b70b4741a00bfe692751f04a9ca73c867c7114c
Teddayz <mervynneo02@gmail.com>
2025-05-10 00:36:15 +0800

    Add Binomial Lattice model logic

--- src/main/java/BinomialLatticeModel.java
+++ src/main/java/BinomialLatticeModel.java
@@ -1,29 +1,47 @@
+import model.option.CallOption;
+import model.option.Option;
+import model.option.PutOption;
 import model.parameters.RiskFreeRate;
 import model.parameters.TimePeriod;
 import model.parameters.Volatility;
 
 public class BinomialLatticeModel {
 
+    private final double initialStockPrice;
     private final RiskFreeRate rfr;
     private final Volatility vpa;
     private final TimePeriod timePeriod;
+    private final Option option;
+    private double upFactor;
+    private double downFactor;
 
-//    private final float upFactor;
-//    private final float downFactor;
-
-    public BinomialLatticeModel(double rfr, double vpa, int years, int months) {
+    public BinomialLatticeModel(double initialStockPrice, double rfr,
+                                double vpa, int years, int months, Option option) {
+        this.initialStockPrice = initialStockPrice;
         this.rfr = new RiskFreeRate(rfr);
         this.timePeriod = new TimePeriod(years, months);
         this.vpa = new Volatility(vpa, this.timePeriod);
+        this.option = option;
+        this.upFactor = Math.exp(this.vpa.getVolatility());
+        this.downFactor = Math.exp(-1 * this.vpa.getVolatility());
     }
-    
+
+    public double getOptionPrice() {
+        double riskFreeProbability = (rfr.getRate() - downFactor) / (upFactor - downFactor);
+        double upStockPrice = this.initialStockPrice * upFactor;
+        double upPayoff = option.calculatePayoff(upStockPrice);
+        double downStockPrice = this.initialStockPrice * downFactor;
+        double downPayoff = option.calculatePayoff(downStockPrice);
+        return (1 / rfr.getRate()) * ((riskFreeProbability * upPayoff) + ((1 - riskFreeProbability) * downPayoff));
+    }
 
     @Override
     public String toString() {
         return "Parameters: " + this.rfr.toString() + this.vpa.toString() + " " + this.timePeriod.toString();
     }
     public static void main(String[] args) {
-        BinomialLatticeModel binomialLatticeModel = new BinomialLatticeModel(5.0, 20.0, 3, 5);
-        System.out.println(binomialLatticeModel.toString());
+        BinomialLatticeModel binomialLatticeModel = new BinomialLatticeModel(50, 0.05,
+                0.25, 1, 0, new PutOption(47));
+        System.out.println(binomialLatticeModel.getOptionPrice());
     }
 }
