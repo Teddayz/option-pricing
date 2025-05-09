1f86591e819e6b9be346b0fa122272a7b8b31d50
Teddayz <mervynneo02@gmail.com>
2025-05-09 13:00:47 +0800

    Add gradle support

---
+++ src/test/java/model/option/CallOptionTest.java
@@ -0,0 +1,24 @@
+package model.option;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CallOptionTest {
+    @Test
+    public void calculatePayoffReturnsCorrectOutput() {
+        CallOption callOption = new CallOption(50.0);
+
+        double stockPrice = 50.0;
+        assertEquals(callOption.calculatePayoff(stockPrice), 0.0);
+
+        stockPrice = 60.0;
+        assertEquals(callOption.calculatePayoff(stockPrice), 10.0);
+
+        stockPrice = 40.0;
+        assertEquals(callOption.calculatePayoff(stockPrice), 0.0);
+
+    }
+}
---
+++ src/test/java/model/option/PutOptionTest.java
@@ -0,0 +1,5 @@
+package model.option;
+
+public class PutOptionTest {
+    
+}
